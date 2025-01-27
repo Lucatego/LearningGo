213d3a11c51bb9894291ea46a4044fb3503042e3
Lucatego <lucasalvites@gmail.com>
2025-01-26 23:27:34 -0500

    QuizGame - Server & Client

--- QuizGame/src/main.go
+++ QuizGame/src/main.go
@@ -23,6 +23,8 @@ var (
 )
 
 func server() {
+	// To send the end signal
+	defer wg.Done()
 	// 1.
 	var file *os.File
 	var err error
@@ -31,10 +33,77 @@ func server() {
 		end <- struct{}{}
 		panic(err)
 	}
+	// Here we use slices since we don´t know the size of the CSV file
+	var items []string
+	var reader *bufio.Scanner = bufio.NewScanner(file)
+	// Loop for all the CSV file
+	for {
+		// Get a token (line)
+		items = funcs.ReadCSVLineToDisplay(reader)
+		// End case
+		if items == nil {
+			displayMutex.Lock()
+			fmt.Println("End of reading")
+			displayMutex.Unlock()
+			qNa <- nil
+			return
+		}
+		// Send it to the channel
+		select {
+		case qNa <- items:
+			displayMutex.Lock()
+			fmt.Println("Question send to client")
+			displayMutex.Unlock()
+		default:
+			displayMutex.Lock()
+			fmt.Println("Waiting for client to free queue")
+			displayMutex.Unlock()
+			time.Sleep(5 * time.Second)
+		}
+	}
 }
 
-func client() {
-
+func client(correct *uint32, total *uint32) {
+	// To send the end signal
+	defer wg.Done()
+	// To read from server and user
+	var items []string
+	var correctAnswer, userAnswer int
+	// 2.
+	for {
+		select {
+		case items = <-qNa:
+			if items == nil {
+				displayMutex.Lock()
+				fmt.Println("End of the quiz")
+				displayMutex.Unlock()
+				return
+			}
+			// Question recieved
+			(*total)++
+			displayMutex.Lock()
+			fmt.Printf("Question #%s:\n", items[question])
+			correctAnswer, _ = strconv.Atoi(items[answer]) // TODO: Handle this error
+			// 3.
+			fmt.Printf("Your answer is: ")
+			fmt.Scanln(&userAnswer)
+			if userAnswer == correctAnswer {
+				(*correct)++
+				fmt.Println("Correct!")
+			} else {
+				fmt.Printf("The correct answer is: %d\n", correctAnswer)
+			}
+			displayMutex.Unlock()
+		case <-end:
+			fmt.Println("Fatal error from server")
+			return
+		default:
+			displayMutex.Lock()
+			fmt.Println("Waiting for a question from \"server\"")
+			displayMutex.Unlock()
+			time.Sleep(1 * time.Second)
+		}
+	}
 }
 
 func main() {
@@ -43,93 +112,12 @@ func main() {
 		2. Ask for answer
 		3. Check input with answer on csv
 	*/
-	var file *os.File
-	var err errorz
-	file, err = os.Open("data/problems.csv")
-	if err != nil {
-		end <- struct{}{}
-		panic(err)
-	}
 	// Using goroutines to improve performance, read and show at the same time
 	wg.Add(2)
-	go func() {
-		// Here we use slices since we don´t know the size of the CSV file
-		var items []string
-		var reader *bufio.Scanner = bufio.NewScanner(file)
-		// To send the end signal
-		defer wg.Done()
-		// Loop for all the CSV file
-		for {
-			// Get a token
-			items = funcs.ReadCSVLineToDisplay(reader)
-			// End case
-			if items == nil {
-				displayMutex.Lock()
-				fmt.Println("End of reading")
-				displayMutex.Unlock()
-				qNa <- nil
-				return
-			}
-			// Send it to the channel
-			select {
-			case qNa <- items:
-				displayMutex.Lock()
-				fmt.Println("Question send to client")
-				displayMutex.Unlock()
-			default:
-				displayMutex.Lock()
-				fmt.Println("Waiting for client to free queue")
-				displayMutex.Unlock()
-				time.Sleep(5 * time.Second)
-			}
-		}
-	}()
-
+	go server()
 	// Using a goroutine to recive the items slice of strings to display
 	var correct, total uint32 = 0, 0
-	go func() {
-		var items []string
-		var correctAnswer int
-		var userAnswer int
-		// To send the end signal
-		defer wg.Done()
-		// Now we wait for the items to be filled by the previous goroutine
-		for {
-			select {
-			case items = <-qNa:
-				if items == nil {
-					displayMutex.Lock()
-					fmt.Println("End of the quz")
-					displayMutex.Unlock()
-					return
-				}
-				// Question recieved
-				total++
-				displayMutex.Lock()
-				fmt.Printf("Question #%s:\n", items[question])
-				correctAnswer, _ = strconv.Atoi(items[answer]) // TODO: Handle this error
-				// Read from user
-				fmt.Printf("Your answer is: ")
-				fmt.Scanf("%d", &userAnswer)
-				if userAnswer == correctAnswer {
-					correct++
-					fmt.Println("Correct!")
-				} else {
-					fmt.Printf("The correct answer is: %d\n", correctAnswer)
-				}
-				displayMutex.Unlock()
-			case <-end:
-				displayMutex.Lock()
-				fmt.Println("Error from server")
-				displayMutex.Unlock()
-			default:
-				displayMutex.Lock()
-				fmt.Println("Waiting for a question from \"server\"")
-				displayMutex.Unlock()
-				time.Sleep(2 * time.Second)
-			}
-		}
-	}()
+	go client(&correct, &total)
 	// To end
 	for i := 0; i < 2; i++ {
 		wg.Wait()
