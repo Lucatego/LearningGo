068460a8b0685e91b8f9affa5b50db7cd4d60805
Lucatego <lucasalvites@gmail.com>
2025-02-13 14:54:19 -0500

    changes on client.go now client validates if the server is up

--- Calculator/src/client/client.go
+++ Calculator/src/client/client.go
@@ -1,25 +1,63 @@
 package main
 
+/*
+	Important: In Go, slices, maps, interfaces are implicit pointers. On the other hand, small data types like uint8,
+	float32 or struct are copied.
+	Be careful with structs that have pointers like string:
+
+	type string struct {
+		data *byte  // Pointer
+		len  int
+	}
+
+	In this case, by passing string as an argument, it copies everything, so if the data is modified, it will only
+	affect to that copy of the pointer but not the original one.
+*/
+
 import (
 	"bufio"
 	"fmt"
 	"net"
 	"os"
 	"strings"
+	"time"
 )
 
-func main() {
-	// Open a connection
-	con, err := net.Dial("tcp", "127.0.0.1:1234")
-	if err != nil {
-		panic(err)
-	}
-	defer con.Close()
+func printHeader() {
+	fmt.Println("-----------------------------------")
+	fmt.Printf("%20s\n", "The calculator")
+	fmt.Printf("usage: <FirstNumber> <operator> <SecondNumber>\n")
+	fmt.Print("Operators: +  -  *  /  ^\n")
+}
 
+func main() {
+	var err error = nil
+	var con net.Conn
+	// Open a connection
+	for {
+		fmt.Println("Trying to connect...")
+		con, err = net.Dial("tcp", "127.0.0.1:1234")
+		if err != nil {
+			fmt.Println("Error connecting: ", err)
+			fmt.Println("Trying again in 5 seconds...")
+			time.Sleep(5 * time.Second)
+		} else {
+			fmt.Println("Connection successful...")
+			break
+		}
+	}
+	defer func() {
+		err = con.Close()
+		if err != nil {
+			panic(err)
+		}
+	}()
+
+	// Readers for user input and server output
 	reader := bufio.NewReader(os.Stdin)
 	response := bufio.NewReader(con)
 
-	fmt.Print("The calculator:\n usage: <FirstNumber> <operator> <SecondNumber>\n Operators: +  -  *  /  ^\n")
+	printHeader()
 
 	for {
 		fmt.Print("Enter your operation: ")
@@ -33,7 +71,7 @@ func main() {
 		}
 
 		ans, _ := response.ReadString('\n')
-		fmt.Print(ans + "\n")
+		fmt.Print("Answer: " + ans + "\n")
 	}
 
 }
