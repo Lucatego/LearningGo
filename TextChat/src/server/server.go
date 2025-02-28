aea319f9753c291f323b15797f46ecdca8254800
Lucatego <lucasalvites@gmail.com>
2025-02-28 11:01:37 -0500

    Server Structure

--- TextChat/src/server/server.go
+++ TextChat/src/server/server.go
@@ -15,11 +15,17 @@ var (
 	// To safely stop the server
 	wg   sync.WaitGroup
 	stop chan struct{} = make(chan struct{})
+	// To manage connections
+	clients chan net.Conn = make(chan net.Conn, queue_max_size)
 	// To print on server console without interfering with the user input
-	output chan string = make(chan string, queue_max_size)
+	output   chan string = make(chan string, queue_max_size)
+	print_mx sync.Mutex
 )
 
 func server(ip_address string, port string) {
+
+	var run_server bool = true
+
 	// Create the socket
 	skt, err := net.Listen(protocol, ip_address+":"+port)
 	if err != nil {
@@ -32,10 +38,12 @@ func server(ip_address string, port string) {
 	go server_console_input()
 
 	// Start listening for clients
-	for {
+	for run_server {
+		// Listen
 		select {
 		case <-stop:
-			break
+			run_server = false
+			stop <- struct{}{}
 		default:
 			// Accept a client and validate
 			conn, err := skt.Accept()
@@ -60,14 +68,32 @@ func server(ip_address string, port string) {
  * It handles commands like "stop" to stop accepting connections.
  */
 func server_console_input() {
-	fmt.Printf("> ")
+	// Open a terminal
+	var input string
+	for {
+		fmt.Printf("> ")
+		fmt.Scanln(&input)
+	}
 }
 
 func safe_print() {
-
+	print_mx.Lock()
+	
+	print_mx.Unlock()
 }
 
 func handle_client(conn net.Conn) {
-	wg.Add(1)
-	defer wg.Done()
+	for {
+		var conn net.Conn = nil
+
+		select {
+		case conn = <-clients:
+			// To sync
+			wg.Add(1)
+			defer wg.Done()
+
+		default:
+
+		}
+	}
 }
