9ec9558681e686e4d703ff92a2495f2e72f71060
Lucatego <lucasalvites@gmail.com>
2025-03-06 12:10:24 -0500

    Client distribution

--- TextChat/src/server/server.go
+++ TextChat/src/server/server.go
@@ -7,43 +7,48 @@ import (
 )
 
 const (
-	protocol       string = "tcp"
-	queue_max_size uint32 = 1024
+	protocol             string = "tcp"
+	queueMaxSize         uint32 = 1024
+	numberOfHandlers     uint8  = 8
+	maxClientsPerHandler uint32 = 64
 )
 
 var (
 	// To safely stop the server
 	wg   sync.WaitGroup
-	stop chan struct{} = make(chan struct{})
+	stop chan struct{} = make(chan struct{}, 1)
 	// To manage connections
-	clients chan net.Conn = make(chan net.Conn, queue_max_size)
+	clients     chan net.Conn = make(chan net.Conn, queueMaxSize)
+	fullHandler chan uint8    = make(chan uint8, numberOfHandlers)
 	// To print on server console without interfering with the user input
-	output   chan string = make(chan string, queue_max_size)
-	print_mx sync.Mutex
+	output  chan string = make(chan string, queueMaxSize)
+	printMx sync.Mutex
 )
 
-func server(ip_address string, port string) {
+func server(ipAddress string, port string) {
 
-	var run_server bool = true
+	var runServer bool = true
 
 	// Create the socket
-	skt, err := net.Listen(protocol, ip_address+":"+port)
+	skt, err := net.Listen(protocol, ipAddress+":"+port)
 	if err != nil {
 		panic(err)
 		return
 	}
 	fmt.Printf("Server listening on %s\n", skt.Addr().String())
 
-	// Console input
-	go server_console_input()
+	// TODO: Console input
+	// go ConsoleInput()
+
+	// Handle the client in other process
+	go DistributeClients()
 
 	// Start listening for clients
-	for run_server {
+	for runServer {
 		// Listen
 		select {
 		case <-stop:
-			run_server = false
-			stop <- struct{}{}
+			runServer = false
 		default:
 			// Accept a client and validate
 			conn, err := skt.Accept()
@@ -53,8 +58,8 @@ func server(ip_address string, port string) {
 			}
 			fmt.Printf("New connection from %s\n", conn.RemoteAddr().String())
 
-			// Handle the client
-			go handle_client(conn)
+			// Send the connection to a channel
+			clients <- conn
 		}
 	}
 
@@ -63,11 +68,9 @@ func server(ip_address string, port string) {
 	skt.Close()
 }
 
-/*
- * This function works as an input for the server.
- * It handles commands like "stop" to stop accepting connections.
- */
-func server_console_input() {
+// ConsoleInput()
+// works as an input for the server. It handles commands like "stop" to stop accepting connections.
+func ConsoleInput() {
 	// Open a terminal
 	var input string
 	for {
@@ -76,24 +79,58 @@ func server_console_input() {
 	}
 }
 
-func safe_print() {
-	print_mx.Lock()
-
-	print_mx.Unlock()
+func SafePrint(output string) {
+	printMx.Lock()
+	fmt.Printf("%s", output)
+	printMx.Unlock()
 }
 
-func handle_client(conn net.Conn) {
+func DistributeClients() {
+	// Here we will receive the connections from the channel to evaluate and distribute them.
+	var handlerListening [numberOfHandlers]bool
+	activeHandlers := numberOfHandlers
+	// Forever loop
+	for i := uint8(0); i < numberOfHandlers; i++ {
+		go HandleClients(i)
+		handlerListening[i] = true
+	}
+
+	// Status control
 	for {
-		var conn net.Conn = nil
-
+		if activeHandlers == 0 {
+			break
+		}
 		select {
-		case conn = <-clients:
-			// To sync
-			wg.Add(1)
-			defer wg.Done()
-
-		default:
-
+		case id := <-fullHandler:
+			handlerListening[id] = false
+			activeHandlers--
 		}
 	}
+}
+
+func HandleClients(id uint8) {
+	numberOfClients := uint32(0)
+
+	// Welcome
+	fmt.Println("Server id: %d\n", id)
+
+	// For the moment, if the server is full, it breaks (Temporal solution).
+	// In a real solution, the DistributeClients() function must create more handlers
+	for {
+		select {
+		case conn := <-clients:
+			// If the handler is full, then give to another
+			if numberOfClients == maxClientsPerHandler {
+				clients <- conn
+				fullHandler <- id
+				break
+			}
+
+			ServeClient(conn)
+		}
+	}
+}
+
+func ServeClient(conn net.Conn) {
+	// Look in the database
 }
